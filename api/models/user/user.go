a0c77e39abd28e8b38602ec196bcdc9900024f0e
NetKBs <abijose535@gmail.com>
2024-04-07 15:32:07 -0400

    feat(models): add new user input models

--- api/models/user/user.go
+++ api/models/user/user.go
@@ -7,5 +7,22 @@ type User struct {
 	Username string `gorm:"unique"`
 	Password string
 	RoleId   uint
-	//Role     Role
+	Role     Role
+}
+
+type UserInputCreate struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	RoleId   uint   `json:"role_id" binding:"required"`
+}
+
+type UserInputUpdate struct {
+	Id       uint   `json:"id" binding:"required"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	RoleId   uint   `json:"role_id"`
+}
+
+type UserInputDelete struct {
+	Id uint `json:"id" binding:"required"`
 }
