b7333d44730659c3922d4943a24bd6454f73f98d
NetKBs <abijose535@gmail.com>
2024-04-07 15:27:01 -0400

    feat(user/interfaces): create user repository interface

--- api/repositories/user/interfaces/user_repository.go
+++ api/repositories/user/interfaces/user_repository.go
@@ -5,6 +5,7 @@ import "github.com/NetKBs/uneg-sase-go-backend/api/models/user"
 type IUserRepository interface {
 	Create(user user.User) (user.User, error)
 	Get(id uint) (user.User, error)
+	GetAll() ([]user.User, error)
 	Update(role user.User) (user.User, error)
 	Delete(id uint) error
 }
